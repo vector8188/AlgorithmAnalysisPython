7ac5071c8ff3cf544aef81357c73bf25f5b2b77a
vaibhav-rbs <vaibhav.rbs@gmail.com>
2017-08-20 10:03:37 -0700

    WIP commit

---
+++ abstarct.py
@@ -0,0 +1,52 @@
+from abc import ABCMeta, abstractmethod
+class Vehicle(object):
+
+    __metaclass__ = ABCMeta
+
+    base_sale_price = 0
+    wheels = 0
+
+    def __init__(self, miles, make, model, year, sold_on):
+        self.miles = miles
+        self.make = make
+        self.model = model
+        self.year = year
+        self.sold_on = sold_on
+
+    def sale_price(self):
+        if self.sold_on is not None:
+            return 0.0
+        return 5000.0 * self.wheels
+
+    def purchase_price(self):
+        """Return the price for which we would pay to purchase the vehicle."""
+        if self.sold_on is None:
+            return 0.0  # Not yet sold
+        return self.base_sale_price - (.10 * self.miles)
+
+    @abstractmethod
+    def vehicle_type(self):
+        """"Return a string representing the type of vehicle this is."""
+        pass
+
+class Car(Vehicle):
+    """A car for sale by Jeffco Car Dealership."""
+
+    base_sale_price = 8000
+    wheels = 4
+
+    def vehicle_type(self):
+        """"Return a string representing the type of vehicle this is."""
+        return 'car'
+
+class Truck(Vehicle):
+    """A truck for sale by Jeffco Car Dealership."""
+
+    base_sale_price = 10000
+    wheels = 4
+
+    def vehicle_type(self):
+        """"Return a string representing the type of vehicle this is."""
+        return 'truck'
+
+c = Car(4, 'Honda', 'Accord', 2014, None)
